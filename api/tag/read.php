9e684e662192fd80a35e9e52602f35b3567f208b
MaysNajjar <56773602+mays-najjar@users.noreply.github.com>
2023-05-03 17:47:29 +0300

    Update read.php

--- api/tag/read.php
+++ api/tag/read.php
@@ -19,26 +19,28 @@
   // Get row count
   $num = $result->rowCount();
 // Check if any tag
+// Check if any tag
 if ($num > 0) {
-  // tag array
-  $tag_arr = array();
+    // Loop through the tags and encode each one separately
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
 
-  while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-      extract($row);
+        $tag_item = array(
+            'tag_id' => $tag_id,
+            'tag_name' => $tag_name
+        );
 
-      $tag_item = array(
-          'tag_id' => $tag_id,
-          'tag_name' => $tag_name
-      );
+        // Encode the tag item as a JSON object
+        echo json_encode($tag_item);
 
-      // Push to array
-      array_push($tag_arr, $tag_item);
-  }
-
-  // Encode the array without the "data" key
-  echo json_encode($tag_arr);
+        // Add a comma separator if not the last item
+        if ($row !== $result->fetch(PDO::FETCH_ASSOC)) {
+            echo ",";
+        }
+    }
 } else {
-  // No tags found
-  echo json_encode(array('message' => 'No tag found'));
+    // No tags found
+    echo json_encode(array('message' => 'No tag found'));
 }
+
 
