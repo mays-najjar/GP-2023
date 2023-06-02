1d4b172a0d60f9b723ca9b1be6c8d02086e3b862
MaysNajjar <56773602+mays-najjar@users.noreply.github.com>
2023-06-02 04:21:45 +0300

    Update index.php

--- index.php
+++ index.php
@@ -351,6 +351,7 @@
                     
                   
                
+               
           <script> 
             //   var properties=document.getElementById("element_properties");
             // var style=document.getElementById("style");
@@ -369,86 +370,10 @@
             </script>   
   
 
-  <div id="element_properties" style="display: none;">   pro      </div>
- <div id="style">
- <div class="input-group mb-3" >
-  <div class="input-group-prepend"style="display: inline-block; margin-right:10px;">
-    <label class="input-group-text" for="inputGroupSelect01" >Display</label>
-  </div>
-  <select class="custom-select " id="inputGroupSelect01" style="display: inline-block;">
-    <option value="block" selected>block</option>
-    <option value="inline">inline</option>
-    <option value="inline-block">inline-block</option>
-    <option value="none">none</option>
-  </select>
-</div>
+  <div id="element_properties" style="display: none;">   Proparities     </div>
+  <div id="style" style="display: inline-block;">   Style     </div>
 
-<div class="input-group mb-3">
-  <div class="input-group-prepend" style="display: inline-block; margin-right: 10px;">
-    <label class="input-group-text" for="inputGroupSelect02">Border Color</label>
-  </div>
-
-  <input type="color" class="form-control " id="inputGroupSelect02" style="display: inline-block;">
-</div>
-
-<div class="input-group mb-3" >
-  <div class="input-group-prepend"style="display: inline-block; margin-right:10px;">
-  <label class="input-group-text" for="inputGroupSelect03" >Text-aligen</label>
-  </div>
-  <select class="custom-select " id="inputGroupSelect03" style="display: inline-block;">
-    <option value="center" selected>center</option>
-    <option value="end">end</option>
-    <option value="left">left</option>
-    <option value="right">right</option>
-  </select>
-</div>
-
-<div class="input-group mb-3">
-  <div class="input-group-prepend" style="display: inline-block; margin-right:10px;">
-    <label class="input-group-text" for="inputGroupSelect04">Padding</label>
-  </div>
-  <input type="number" value="1" class="custom-select" id="inputGroupSelect04" style="display: inline-block; width: 50px; ">
-  <select id="unitSelect" class="custom-select" style="display: inline-block; margin-left:5px; ">
-    <option value="px" selected>px</option>
-    <option value="%">%</option>
-    <option value="rem">rem</option>
-  </select>
-</div>
-
-<div class="input-group mb-3">
-  <div class="input-group-prepend" style="display: inline-block; margin-right:10px;">
-    <label class="input-group-text" for="inputGroupSelect05">Margin</label>
-  </div>
-  <input type="number" value="1" class="custom-select" id="inputGroupSelect05" style="display: inline-block; width: 50px; ">
-  <select id="unitSelect" class="custom-select" style="display: inline-block; margin-left:5px; ">
-    <option value="px" selected>px</option>
-    <option value="%">%</option>
-    <option value="rem">rem</option>
-  </select>
-</div>
-<div class="input-group mb-3">
-  <div class="input-group-prepend" style="display: inline-block; margin-right: 10px;">
-    <label class="input-group-text" for="inputGroupSelect06">Background Color</label>
-  </div>
-
-  <input type="color" class="form-control " id="inputGroupSelect06" style="display: inline-block;">
-</div>
-<div class="input-group mb-3">
-  <div class="input-group-prepend" style="display: inline-block; margin-right: 10px;">
-    <label class="input-group-text" for="inputGroupSelect07">Text Color</label>
-  </div>
-
-  <input type="color" class="form-control " id="inputGroupSelect07" style="display: inline-block;">
-</div>
-<!-- Add an onclick event to the "Save" button -->
-<button type="button" class="btn btn-primary" id="saveButton" onclick="saveData()">Save</button>
-
-<script>
-
-
-
-</script> 
-</div>
+ 
             <!-- <iframe src="properties_info.php" title="properties_info" class="col-xs-12" name="my-iframe">
 
 </iframe> -->
